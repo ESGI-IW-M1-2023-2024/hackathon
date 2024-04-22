46c27fb58b549e30d93ba7d98f8f65e66ab5101d
Mathis ROME <mathis.rome@icloud.com>
2024-04-22 19:30:26 +0200

    feat(region): Controller region + Service Pagination

--- api/src/Entity/Region.php
+++ api/src/Entity/Region.php
@@ -6,9 +6,12 @@ use App\Repository\RegionRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Serializer\Attribute\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: RegionRepository::class)]
+#[UniqueEntity(["country", "label"], groups: ["region:new", "region:edit"])]
 class Region
 {
     #[ORM\Id]
@@ -19,10 +22,13 @@ class Region
 
     #[ORM\Column(length: 255)]
     #[Groups(["region:read"])]
+    #[Assert\NotBlank(groups: ["region:new"])]
     private ?string $label = null;
 
     #[ORM\Column(length: 255)]
     #[Groups(["region:read"])]
+    #[Assert\NotBlank(groups: ["region:new"])]
+    #[Assert\Country(groups: ["region:new", "region:edit"])]
     private ?string $country = null;
 
     /**
@@ -30,6 +36,10 @@ class Region
      */
     #[ORM\OneToMany(targetEntity: Wine::class, mappedBy: 'region')]
     private Collection $wines;
+
+    #[ORM\Column]
+    #[Groups(["region:read"])]
+    private bool $archived = false;
 
     public function __construct()
     {
@@ -91,6 +101,18 @@ class Region
                 $wine->setRegion(null);
             }
         }
+
+        return $this;
+    }
+
+    public function isArchived(): bool
+    {
+        return $this->archived;
+    }
+
+    public function setArchived(bool $archived): static
+    {
+        $this->archived = $archived;
 
         return $this;
     }
