c04f7b917ea02cbc5694efe9403757f1eab33f3c
BlueSkunka <quentin.somveille3874@gmail.com>
2024-04-23 18:13:36 +0200

    feat: mailer base

--- api/src/Service/MailerService.php
+++ api/src/Service/MailerService.php
@@ -2,17 +2,26 @@
 
 namespace App\Service;
 
+use EnvProcessorService;
 use MailerEnum;
 use Symfony\Component\Mailer\MailerInterface;
 
 class MailerService
 {
+    // Email pour l'envoie des mails
+    private $sender;
+
     public function __construct(
-        private MailerInterface $mailerInterface
+        private MailerInterface $mailerInterface,
     ) {
+        $this->sender = getenv('MAILER_SENDER');
     }
 
-    public function sendMail(MailerEnum $mailerEnum, array $options)
+    /**
+     * Pour chaque mail à envoyer créer la fonction correspondante et l'appeler directement depuis le code
+     * Cette fonction ne sert qu'a lister les emails possibles
+     */
+    public function sendMail(MailerEnum $mailerEnum)
     {
         match ($mailerEnum) {
             MailerEnum::BOOKING => null,
