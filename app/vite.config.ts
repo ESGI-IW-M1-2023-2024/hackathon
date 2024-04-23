aadaa1c42fd2b5d78f47968803b884a28df520b4
Olivier PERDRIX <olivierperdrix@live.fr>
2024-04-23 12:24:08 +0200

    test: :sparkles: Add @ alias into vite and tsconfig for imports

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default ({ mode }: { mode: string }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
@@ -25,6 +26,11 @@ export default ({ mode }: { mode: string }) => {
             }
           },
         },
+      },
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
       },
     },
   });
