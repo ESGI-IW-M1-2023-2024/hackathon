cde4b1a36cea76407427b176f5eb1c0bf1f27b24
loan <loan.courchinoux-billonnet@koesio.com>
2024-04-23 12:08:41 +0200

    feat: :sparkles: notification logic

--- app/src/pages/auth/login.page.tsx
+++ app/src/pages/auth/login.page.tsx
@@ -2,8 +2,12 @@ import { ChangeEvent, useState } from 'react';
 import { UserCredentials } from '../../features/auth/types/logged-user.type';
 import { Button, TextField } from '@mui/material';
 import { apiSlice } from '../../redux/api/api.slice';
+import { useAppDispatch } from '../../redux/hooks';
+import { openSnackBar } from '../../redux/slices/notification.slice';
 
 const Login = () => {
+  const dispatch = useAppDispatch();
+
   const [credentials, setCredentials] = useState<UserCredentials>({ username: '', password: '' });
   const [loginUser] = apiSlice.useLoginUserMutation();
 
@@ -11,8 +15,10 @@ const Login = () => {
     try {
       await loginUser(credentials).unwrap();
       console.log('ok');
+      dispatch(openSnackBar({ message: 'Connexion réussi', severity: 'success' }));
     } catch (error: unknown) {
       console.log('pas ok');
+      dispatch(openSnackBar({ message: 'Echec de la connexion', severity: 'error' }));
     }
   };
 
--- app/src/redux/store.tsx
+++ app/src/redux/store.tsx
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/user.slice';
+import notificationReducer from './slices/notification.slice';
 import { apiSlice } from './api/api.slice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    notification: notificationReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   devTools: import.meta.env.NODE_ENV !== 'production',
