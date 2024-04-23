473940e9ec5332e56b7ec056619a57170d0317c0
Arnaud Gouel <gouel.arnaud@gmail.com>
2024-04-23 15:06:56 +0200

    fix (entities): adjust entities according to dbdiagram

--- api/src/Entity/Theme.php
+++ api/src/Entity/Theme.php
@@ -5,8 +5,10 @@ namespace App\Entity;
 use App\Repository\ThemeRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Attribute\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ThemeRepository::class)]
 class Theme
@@ -19,13 +21,25 @@ class Theme
 
     #[ORM\Column(length: 255)]
     #[Groups(["workshop:read"])]
+    #[Assert\NotBlank(groups: ["theme:new", "theme:edit"])]
     private ?string $label = null;
-
     /**
      * @var Collection<int, Workshop>
      */
     #[ORM\OneToMany(targetEntity: Workshop::class, mappedBy: 'theme')]
     private Collection $workshops;
+
+    #[ORM\Column(type: Types::TEXT)]
+    #[Groups(["workshop:read"])]
+    #[Assert\NotBlank(groups: ["theme:new", "theme:edit"])]
+    private ?string $content = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(["theme:read", "workshop:read"])]
+    private ?string $subtitle = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $headerFilename = null;
 
     public function __construct()
     {
@@ -75,6 +89,42 @@ class Theme
                 $workshop->setTheme(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getSubtitle(): ?string
+    {
+        return $this->subtitle;
+    }
+
+    public function setSubtitle(string $subtitle): static
+    {
+        $this->subtitle = $subtitle;
+
+        return $this;
+    }
+
+    public function getHeaderFilename(): ?string
+    {
+        return $this->headerFilename;
+    }
+
+    public function setHeaderFilename(string $headerFilename): static
+    {
+        $this->headerFilename = $headerFilename;
 
         return $this;
     }
