d65884043b33e043c8155039f5306c2d8d1a562f
loan <loan.courchinoux-billonnet@koesio.com>
2024-04-23 17:09:55 +0200

    feat: :children_crossing: show warning message if access to logged page

--- app/src/features/auth/components/secure-route.tsx
+++ app/src/features/auth/components/secure-route.tsx
@@ -1,13 +1,16 @@
-import { useAppSelector } from '@/redux/hooks';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { LoggedUser } from '../types/logged-user.type';
 import { RootState } from '@/redux/store';
 import { Navigate, Outlet } from 'react-router-dom';
 import { LinearProgress } from '@mui/material';
+import { openSnackBar } from '@/redux/slices/notification.slice';
 
 const SecureRoute = () => {
   const user: LoggedUser | null = useAppSelector((state: RootState) => state.user);
+  const dispatch = useAppDispatch();
 
   if (!user) {
+    dispatch(openSnackBar({ message: 'Vous devez être connecté pour accéder à cette page', severity: 'warning' }));
     return <Navigate to='/login' />;
   }
 
