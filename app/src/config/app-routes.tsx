53307dc461ecf56b79265061122a3df91bcffd7d
Mathis ROME <mathis.rome@icloud.com>
2024-04-24 12:42:50 +0200

    wip(region-crud): Liste et ajout des régions

--- app/src/config/app-routes.tsx
+++ app/src/config/app-routes.tsx
@@ -5,6 +5,8 @@ import NotFound from '../pages/common/not-found.page';
 import SecureRoute from '@/features/auth/components/secure-route';
 import Dashboard from '@/pages/admin/dashboard.page';
 import ThemesList from '@/pages/admin/themes-list.page';
+import RegionList from "@/pages/admin/region/list.page";
+import CreateRegion from "@/pages/admin/region/create.page";
 import EditTheme from '@/pages/admin/themes-edit.page';
 import CreateTheme from '@/pages/admin/themes-create.page';
 
@@ -17,6 +19,8 @@ const AppRoutes = () => (
       <Route path='/themes' element={<ThemesList />} />
       <Route path='/themes/create' element={<CreateTheme />} />
       <Route path='/themes/:id' element={<EditTheme />} />
+        <Route path='/admin/regions' element={<RegionList />} />
+        <Route path='/admin/regions/create' element={<CreateRegion />} />
     </Route>
     <Route path='*' element={<NotFound />} />
   </Routes>
