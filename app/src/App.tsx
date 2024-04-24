b979951adbd491dffb4fb402ee041f61e1b9c45b
Olivier Perdrix <olivierperdrix@live.fr>
2024-04-24 20:33:43 +0200

    feat(navbar): :sparkles: add new responsive navbar

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,12 +4,14 @@ import Notification from './features/UI/common/components/notification.component
 import AppRoutes from './config/app-routes';
 import Navbar from './features/UI/common/components/navbar';
 import Footer from './features/UI/common/components/footer';
+import MainNavigation from './features/UI/common/components/mainNavigation';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Navbar />
+      {/* <Navbar /> */}
+      <MainNavigation />
       <AppRoutes />
       <Footer />
       <Notification />
