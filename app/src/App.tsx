c136f34fc9e1cd0f246c386440f42b6b80707222
Olivier PERDRIX <olivierperdrix@live.fr>
2024-04-24 16:43:43 +0200

    * feat(App.tsx): add Navbar and Footer components to App component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,12 +2,16 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from './styles/mui/main-theme';
 import Notification from './features/UI/common/components/notification.component';
 import AppRoutes from './config/app-routes';
+import Navbar from './features/UI/common/components/navbar';
+import Footer from './features/UI/common/components/footer';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <Navbar />
       <AppRoutes />
+      <Footer />
       <Notification />
     </ThemeProvider>
   );
