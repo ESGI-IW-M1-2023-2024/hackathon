e84aa4bdea95762e50e6f0251fcf8bf9c9b4de47
Olivier PERDRIX <olivierperdrix@live.fr>
2024-04-25 14:32:56 +0200

    fix(winepage): :zap: change colors

--- app/src/pages/common/wine.page.tsx
+++ app/src/pages/common/wine.page.tsx
@@ -19,9 +19,6 @@ const Wine = () => {
                 justifyContent='center'
                 alignItems='center'
                 height='100vh'
-                sx={{
-                    backgroundColor: '#B9515B',
-                }}
             >
                 <CircularProgress />
             </Box>
@@ -35,7 +32,6 @@ const Wine = () => {
                     sx={{
                         display: 'flex',
                         flexDirection: { xs: 'column', sm: 'row' },
-                        color: 'white',
                         justifyContent: 'space-between',
                         alignItems: { sm: 'center' },
                         width: '100%',
@@ -46,6 +42,7 @@ const Wine = () => {
                         sx={{
                             flex: '1 1 auto',
                             fontWeight: 'bold',
+                            color: props.isHeading ? 'secondary.main' : 'text.primary',
                         }}
                     >
                         {props.isHeading ? props.value : props.title}
@@ -126,146 +123,141 @@ const Wine = () => {
         };
 
     return (
-        <Stack sx={
-            {
-
-                backgroundColor: "#B9515B",
-                width: '100vw',
-            }
-        }>
-            <Container
+        <Container
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'flex-start',
+                alignItems: 'center',
+                padding: '2rem',
+                //minHeight: '100vh',
+            }}
+        >
+            {/* Description du vin */}
+            <Stack
+                direction={{ xs: "column", sm: "row" }}
+                spacing={4}
+                alignItems="flex-start"
+                justifyContent="space-between"
                 sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'flex-start',
-                    alignItems: 'center',
-                    backgroundColor: "#B9515B",
-                    padding: '2rem',
-                    //minHeight: '100vh',
+                    //color: 'white',
+                    mb: 4,
+                    width: '100%',
                 }}
             >
-                {/* Description du vin */}
+                {/* Textes descriptifs */}
                 <Stack
-                    direction={{ xs: "column", sm: "row" }}
-                    spacing={4}
-                    alignItems="flex-start"
-                    justifyContent="space-between"
+                    component='ul'
+                    direction={'column'}
+                    alignItems={'flex-start'}
+                    justifyContent={'flex-start'}
+                    spacing={2}
                     sx={{
-                        color: 'white',
-                        mb: 4,
-                        width: '100%',
+                        width: { xs: '100%', sm: '70%' },
+                        //color: 'white',
                     }}
                 >
-                    {/* Textes descriptifs */}
-                    <Stack
-                        component='ul'
-                        direction={'column'}
-                        alignItems={'flex-start'}
-                        justifyContent={'flex-start'}
-                        spacing={2}
-                        sx={{
-                            width: { xs: '100%', sm: '70%' },
-                            color: 'white',
-                        }}
-                    >
-                        <DescriptionElement title='Nom' value={defaultValues.label} isHeading />
-                        <DescriptionElement title='Région' value={data?.region.label} />
-                        <DescriptionElement title='Cépage' value={defaultValues.grapeVariety} />
-                        <DescriptionElement title='Température de service' value={defaultValues.servingTemperature} />
-                        <DescriptionElement title='Conservation' value={defaultValues.storage} />
-                        <DescriptionElement title='Durée de garde' value={defaultValues.upTo} />
-                        <DescriptionElement title='Goûts' value={defaultValues.taste} />
-                        <DescriptionElement title='Par goûts' value={defaultValues.byTaste} />
-                        <DescriptionElement title='À l’œil' value={defaultValues.byEye} />
-                        <DescriptionElement title='Au nez' value={defaultValues.onTheNose} />
-                        <DescriptionElement title='En bouche' value={defaultValues.inTheMouth} />
-                        <DescriptionElement title='Accords mets-vins' value={defaultValues.winePairing} />
-                        <DescriptionElement title='Accords recommandés' value={defaultValues.recommandedPairing} />
-                    </Stack>
-                    {/* Image du vin */}
-                    <Stack
-                        component='figure'
-                        className='d-shadow'
-                        flexDirection={'column'}
-                        sx={{
-                            width: 'auto',
-                            height: 'auto',
-                            '& img': {
-                                width: '100%',
-                                height: { xs: '90vh', sm: 'auto' },
-                                objectFit: 'contain',
-                            },
-                            backgroundColor: theme.palette.secondary.main,
-                            borderRadius: '4px',
-                            alignSelf: 'center',
-                        }}
-                    >
-                        <img className="d-shadow" src={defaultValues.imageFilename || wineImage} alt={defaultValues.label} />
+                    <DescriptionElement title='Nom' value={defaultValues.label} isHeading />
+                    <DescriptionElement title='Région' value={data?.region.label} />
+                    <DescriptionElement title='Cépage' value={defaultValues.grapeVariety} />
+                    <DescriptionElement title='Température de service' value={defaultValues.servingTemperature} />
+                    <DescriptionElement title='Conservation' value={defaultValues.storage} />
+                    <DescriptionElement title='Durée de garde' value={defaultValues.upTo} />
+                    <DescriptionElement title='Goûts' value={defaultValues.taste} />
+                    <DescriptionElement title='Par goûts' value={defaultValues.byTaste} />
+                    <DescriptionElement title='À l’œil' value={defaultValues.byEye} />
+                    <DescriptionElement title='Au nez' value={defaultValues.onTheNose} />
+                    <DescriptionElement title='En bouche' value={defaultValues.inTheMouth} />
+                    <DescriptionElement title='Accords mets-vins' value={defaultValues.winePairing} />
+                    <DescriptionElement title='Accords recommandés' value={defaultValues.recommandedPairing} />
+                </Stack>
+                {/* Image du vin */}
+                <Stack
+                    component='figure'
+                    className='d-shadow'
+                    flexDirection={'column'}
+                    sx={{
+                        width: 'auto',
+                        height: 'auto',
+                        '& img': {
+                            width: '100%',
+                            height: { xs: '90vh', sm: 'auto' },
+                            objectFit: 'contain',
+                        },
+                        backgroundColor: theme.palette.primary.light,
+                        borderRadius: '4px',
+                        alignSelf: 'center',
+                    }}
+                >
+                    <img className="d-shadow" src={defaultValues.imageFilename || wineImage} alt={defaultValues.label} />
 
-                        <Typography
-                            component={'figcaption'}
-                            sx={{
-                                color: 'white',
-                                textAlign: 'center',
-                                padding: '1rem',
-                            }}
-                        >
-                            Quantité : <strong>{defaultValues.quantity}</strong>
-                        </Typography>
-                    </Stack>
-                    {/* Fin de l'image du vin */}
+
                 </Stack>
-                {/* fin de la description du vin */}
-                <Stack>
-                    <Typography
-                        variant='h3'
-                        sx={{
-                            color: 'white',
-                            fontWeight: 'bold',
-                            fontSize: '1.5rem',
-                        }}
-                    >
-                        Commentaires d'Olivier
-                    </Typography>
-                    <Typography
-                        sx={{
-                            color: 'white',
-                            fontSize: '1rem',
-                        }}
-                    >
-                        {defaultValues.comments}
-                    </Typography>
-                </Stack>
-                <Stack
+                {/* Fin de l'image du vin */}
+            </Stack>
+            {/* fin de la description du vin */}
+            <Stack
+                sx={{
+                    mt: 4,
+                    mb: 4,
+                }}
+            >
+                <Typography
+                    variant='h3'
                     sx={{
-                        mt: 4,
-                        mb: 4,
+                        fontWeight: 'bold',
+                        fontSize: '1.5rem',
                     }}
                 >
-                    <Typography
-                        variant='h3'
-                        sx={{
-                            color: 'white',
-                            fontWeight: 'bold',
-                            fontSize: '1.5rem',
-                        }}
-                    >
-                        Description
-                    </Typography>
-                    <Typography
-                        sx={{
-                            color: 'white',
-                            fontSize: '1rem',
-                        }}
-                    >
-                        {defaultValues.content}
-                    </Typography>
-                </Stack>
-                <Stack>
-                    <ColorButton>Télécharger la fiche technique</ColorButton>
-                </Stack>
-            </Container>
-        </Stack>
+                    Description
+                </Typography>
+                <Divider
+                    sx={{
+                        mt: 1,
+                        mb: 2,
+                    }}
+                />
+                <Typography
+                    sx={{
+                        fontSize: '1rem',
+                    }}
+                >
+                    {defaultValues.content}
+                </Typography>
+            </Stack>
+            <Stack
+                sx={{
+                    mt: 4,
+                    mb: 4,
+                }}
+            >
+                <Typography
+                    variant='h3'
+                    sx={{
+                        fontWeight: 'bold',
+                        fontSize: '1.5rem',
+                    }}
+                >
+                    Commentaires d'Olivier
+                </Typography>
+                <Divider
+                    sx={{
+                        mt: 1,
+                        mb: 2,
+                    }}
+                />
+                <Typography
+                    sx={{
+                        fontSize: '1rem',
+                    }}
+                >
+                    {defaultValues.comments}
+                </Typography>
+            </Stack>
+            <Stack>
+                <ColorButton>Télécharger la fiche technique</ColorButton>
+            </Stack>
+        </Container>
     )
 }
 
